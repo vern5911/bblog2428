442dbdca48875ce790d01ee4d7132cded4ee1382
vern5911 <97927230+vern5911@users.noreply.github.com>
2025-09-15 11:13:44 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # app.py : Windows 10 /c/Users/Ed/Desktop/flaskprod/bblog2428/app.py
 #        : Installed gunicorn on 2/7/25
 #        : Updated bblog2428 on Github on 2/7/25
-#
+#        : Backed out the Heroku implementation due to incompatibility and fees
+#        : 06-Jul-25, added get_nextid() to obtain latest editable record as default
+#        :   for index.html.
+#       
 import os
 import sqlite3
-import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, url_for, flash, redirect, abort
+from flask import Flask, render_template, request, url_for, redirect, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, date
 from icecream import ic
@@ -15,13 +17,23 @@ from icecream import ic
 load_dotenv()                       # Load environment variables from .env file
 
 db_name = os.getenv('DATABASE_URL')
-db_url = 'sqlite:///' + db_name
+db_path = 'sqlite:///' + db_name
 secret_key = os.getenv('SECRET_KEY')
 
 # Instantiate the Bibleblog APP
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Readings(db.Model):
+    bid = db.Column(db.Integer, primary_key=True)
+    bdate = db.Column(db.String, nullable=False)
+    book = db.Column(db.String, nullable=False)
+    chapter = db.Column(db.Integer, nullable=False)
+    feedback = db.Column(db.Text)
 
 # Establish connection to the sqlite3 DB
 def get_db_connection():
@@ -30,39 +42,49 @@ def get_db_connection():
     con.row_factory = sqlite3.Row
     return con
 
+# Compute next_id - next unedited Table record
+def get_nextid():
+    # Query DB table for latest unedited record Id (Bid)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    sql_ = """SELECT MIN(Bid) FROM bblog2428 
+                    WHERE Feedback IS NULL 
+                    ORDER BY Bid ASC 
+                    LIMIT 1;"""
+    next_bid = cur.execute(sql_).fetchone()
+    for nid in next_bid:
+        nb = nid 
+    return nb
+
 # Set globals
 Today = date.today()
 iDate = date(2024,12,1)
-Id = (Today - iDate).days + 1
+Id = (Today - iDate).days + 1; 
+ic('Today Id: ',Id)
+ic('Next unedited Id: ', get_nextid())
 
+def form_dt(Bdt):
+    # Create components of the current date
+    yr=int(Bdt[0:4]); mo=int(Bdt[5:7]); dy=int(Bdt[8:10])
+    return date(yr,mo,dy).strftime("%a %Y-%m-%d")
+
+# Obtain the readings for a specific table id value
 def get_readings(r_id):
-    conn = get_db_connection()
-    reads = conn.execute("""SELECT Bid, Bdate, Book, Chapter, Feedback
+    con = get_db_connection()
+    nxt_id = get_nextid()
+    reads = con.execute("""SELECT Bid, Bdate, Book, Chapter, Feedback
                             FROM bblog2428
                             WHERE Bid=?""",(r_id,)).fetchone()
     return reads
 
-def form_dt(Bdt):
-    yr=int(Bdt[0:4]); mo=int(Bdt[5:7]); dy=int(Bdt[8:10])
-    return date(yr,mo,dy).strftime("%a %Y-%m-%d")
-
 # Define all routes for the APP
 @app.route('/')
 def index():
-    global Id
     # Generate default record Id for today's readings
-    reads = get_readings(Id)
+    curr_id = get_nextid()
+    reads = get_readings(curr_id)
     tdate = form_dt(reads['Bdate'])
     return render_template('index.html', reads=reads, tdate=tdate)
-
-@app.route('/<int:Post_Id>/get/', methods=['GET', 'POST'])
-def get(Post_Id):
-    reads = get_reading(Post_Id)
-    conn = get_db_connection()
-    read = conn.execute("""SELECT * FROM bblog2428
-                                    WHERE Bid=?""",(Post_Id,)).fetchone()
-    conn.close()
-    return render_template('get.html', read=read)
 
 # Edit blog post
 @app.route('/<int:Id>/edit/', methods=('GET', 'POST'))
@@ -93,6 +115,8 @@ def edit(Id):
             return redirect(url_for('index'))
     return render_template('edit.html', reads=reads)
 
+
+# Generate a report based on the localhost:5000/yym/report browser input
 @app.route('/<int:yrm>/report')
 def report(yrm):
     yym=str(yrm)
@@ -101,8 +125,9 @@ def report(yrm):
     req_date = date(int(yr), int(mo), 1)
     Month=req_date.strftime('%B %Y')
     conn = get_db_connection()
-    sql_ = """SELECT * FROM bblog2428 WHERE substr(Bdate,1,4)=?
-                        AND substr(Bdate,6,2)=?"""
+    sql_ = """SELECT * FROM bblog2428 
+                        WHERE substr(Bdate,1,4)=? AND substr(Bdate,6,2)=?
+                        ORDER BY Bid ASC """
     reads = conn.execute(sql_,(yr,mo)).fetchall()
     conn.close()
     return render_template('report.html', reads=reads, Month=Month)
@@ -119,8 +144,6 @@ def books():         # Parm = book name from the DB
                                     GROUP BY Book
                                     HAVING count(Feedback)<>'None'
                                     ORDER BY min(Bid)""").fetchall()
-    #for read in reads:
-    #    print(reads[0],read[1],read[2])
     return render_template('books.html', reads=reads, Title=Title)
 
 
